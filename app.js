3ab54a3d1b05761e84ee72f5ebb207cdf883ef9c
copriot <developer.zeki@gmail.com>
2024-04-14 23:11:02 +0300

    some dynamism funcs added but still not fnished

--- app.js
+++ app.js
@@ -14,5 +14,36 @@ key.addEventListener('click', function(e){
     const element = e.target;
     
     if(!element.matches('button'))return; //element buttonla eşleşmiyorsa fonksiyonu durdur
-    console.log(element);
+    if (element.classList.contains('operator')){
+        console.log('operator',element.value);return;
+    };
+    if (element.classList.contains('decimal')){
+        //console.log('decimal',element.value);return;
+        inputDecimal();
+        updateDisplay();
+        return;
+    };
+    if (element.classList.contains('clear')){
+        //console.log('clear',element.value);return;
+        clear();
+        updateDisplay();
+        return;
+    };
+  // console.log('number',element.value);
+  //buttondaki değeri updateDisplay fonksiyonu değerine eşitle
+  inputNumber(element.value);
+  updateDisplay();
 });
+//ilk başta 0 sa sayıyı yaz değilse önceki değerin yanına yeni tıklanan değeri ekle
+function inputNumber(num){
+displayValue = displayValue==='0'? num : displayValue+num;
+};
+function inputDecimal() {
+    
+    if(!displayValue.includes('.')){
+        displayValue += '.';
+    }
+};
+function clear(){
+    displayValue='0';
+}
